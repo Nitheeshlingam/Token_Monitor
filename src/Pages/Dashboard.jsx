89fe61ac1d82ef7a2560b3257b0fa85f95c3a680
AvinashDev018 <ssavinash2006@gmail.com>
2026-07-10 10:21:55 +0530

    Models

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,15 +2,12 @@ import DashboardCards from "../components/DashboardCards";
 import Navbar from "../components/Navbar";
 import DailyUsageChart from "../components/DailyUsageChart";
 function Dashboard() {
-
     return (
-
         <>
             <Navbar />
             <DashboardCards />
             <DailyUsageChart />
         </>
-
     );
 
 }
